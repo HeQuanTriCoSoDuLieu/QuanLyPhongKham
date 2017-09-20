f396cfc698695e80a2dd9ec954c38338c0391f96
dhdatyit.sgu@gmail.com <daty.danghuynh20497@gmail.com>
2017-09-20 16:47:46 +0700

    Đạt Ý - Tạo solution.

---
+++ QuanLyPhongKham/QuanLyPhongKham/DAO/DataProvider.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuanLyPhongKham.DAO
+{
+    public class DataProvider
+    {
+        #region Singletone Parttern
+
+        private static DataProvider instane;
+
+        /// <summary>
+        /// kiểm tra xem đã tạo đối tượng DataProvider nào trước đó hay chưa.
+        /// Nếu chưa thì tạo mới, nếu có rồi thì trả về đối tượng đó lun.
+        /// </summary>
+        public static DataProvider Instane
+        {
+            get
+            {
+                if (instane == null)
+                {
+                    instane = new DataProvider();
+                }
+                return instane;
+            }
+        }
+
+
+        /// <summary>
+        /// private constructor để ngăn việc tạo đối tượng từ bên ngoài
+        /// </summary>
+        private DataProvider() { }
+
+
+        #endregion
+
+        
+        #region
+
+        /// <summary>
+        /// Biến dùng để kết nối đến db
+        /// </summary>
+        private SqlConnection connection;
+
+        /// <summary>
+        /// Connection String dùng để kết nối db
+        /// </summary>
+        private string connectionString = @"Data Source=.;Initial Catalog=quanlyphongkham;Integrated Security=True";
+
+
+        
+
+
+        #endregion
+    }
+}
---
+++ QuanLyPhongKham/QuanLyPhongKham/GUI/fLogin.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QuanLyPhongKham.GUI
+{
+    public partial class fLogin : MetroFramework.Forms.MetroForm
+    {
+        public fLogin()
+        {
+            InitializeComponent();
+        }
+        /// <summary>
+        /// hàm load form khi khởi chạy
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void fLogin_Load(object sender, EventArgs e)
+        {
+            this.StyleManager = metroStyleManager1;
+            metroStyleManager1.Theme = MetroFramework.MetroThemeStyle.Light;
+            metroStyleManager1.Style = MetroFramework.MetroColorStyle.Blue;
+            txbUserName.Focus();
+        }
+
+
+        /// <summary>
+        /// sự kiện cho button đăng nhập
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+
+        }
+
+
+        /// <summary>
+        /// Sự kiện trước khi đóng form
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void fLogin_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (MessageBox.Show("Bạn có thật sự muốn thoát chương trình?", "Thông báo", MessageBoxButtons.YesNo
+                , MessageBoxIcon.Question) == System.Windows.Forms.DialogResult.No)
+            {
+                e.Cancel = true;
+            }
+        }
+    }
+}
